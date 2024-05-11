ceaa8a6eef9c6970af9cd2b80f49ae13123ff10e
Normalnoise <102578774+Normalnoise@users.noreply.github.com>
2024-05-11 15:03:39 +0800

    Update CPAccountUpgradeable.sol

--- hardhat/contracts/CPAccountUpgradeable.sol
+++ hardhat/contracts/CPAccountUpgradeable.sol
@@ -26,7 +26,7 @@ contract CPAccountUpgradeable is Initializable, UUPSUpgradeable {
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event WorkerChanged(address indexed previousWorker, address indexed newWorker);
     event MultiaddrsChanged(string[] newMultiaddrs);
-    event BeneficiaryChanged(address indexd previousBeneficiary, address indexd newBeneficiary);
+    event BeneficiaryChanged(address indexed previousBeneficiary, address indexed newBeneficiary);
     event TaskTypesChanged(uint8[] newTaskTypes); // New event
     event UBIProofSubmitted(address indexed submitter, address taskContract, string taskId, uint8 taskType, string proof); // Changed to 'type'
 
