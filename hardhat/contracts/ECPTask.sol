f6384741425b7a5f6dffc43d467a66b2cfa2b3c2
Normalnoise <102578774+Normalnoise@users.noreply.github.com>
2024-06-18 16:10:36 +0800

    Update ECPTask.sol

--- hardhat/contracts/ECPTask.sol
+++ hardhat/contracts/ECPTask.sol
@@ -1,160 +1,67 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract ECPTask is Ownable {
-
-    struct TaskInfo {
-        uint taskType;
-        uint resourceType;
-        string inputParam;
-        string verifyParam;
-        address cpContractAddress;
-        string status;
-        string rewardTx;
-        string proof;
-        string challengeTx;
-        string lockFundTx;
-        string unlockFundTx;
-        string slashTx;
-        uint deadline;
-        bool isSubmitted;
-        bool isChallenged;
-    }
-
-
-    uint public taskType;
-    uint public resourceType;
+contract ECPTask {
+    // Task properties
+    uint256 public taskID;
+    string public taskType;
+    string public resourceType;
     string public inputParam;
     string public verifyParam;
-    address public cpContractAddress;
-    string public status;
-    string public rewardTx;
+    address public cpAccount;
     string public proof;
-    string public challengeTx;
-    string public lockFundTx;
-    string public unlockFundTx;
-    string public slashTx;
-    uint public deadline;
-    bool public isSubmitted;
-    bool public isChallenged;
+    uint256 public deadline;
+    address public taskRegistryContract;
+    string public checkCode;
 
-    // TaskInfo public taskInfo;
-    mapping(address => bool) public isAdmin;
+    // Contract owner
+    address public owner;
 
-    event RewardAndStatusUpdated(string rewardTx, string status);
-    event LockAndStatusUpdated(address cpContractAddress, string lockFundTx, string status);
-    event UnlockAndStatusUpdated(string unlockFundTx, string status);
-    event ChallengeAndStatusUpdated(string challengeTx, string status);
-    event SlashAndStatusUpdated(string slashTx, string status);
+    // Event to log registration
+    event RegisteredToTaskRegistry(address indexed taskContract, address indexed owner);
 
-    event SubmitProof(string proof);
-
+    // Constructor to initialize the task properties and register the task contract
     constructor(
-        uint _taskType,
-        uint _resourceType,
+        uint256 _taskID,
+        string memory _taskType,
+        string memory _resourceType,
         string memory _inputParam,
         string memory _verifyParam,
-        address _cpContractAddress,
-        string memory _status,
-        string memory _lockFundTx,
-        uint _deadline
-    ) Ownable(msg.sender) {
-
-            require(_taskType > 0, "Invalid task type");
-            require(_resourceType >= 0, "Invalid resource type");
-            require(bytes(_inputParam).length > 0, "Input parameter cannot be empty");
-            require(bytes(_status).length > 0, "Status cannot be empty");
-            require(_deadline > 0, "Deadline must be positive");
-
-            taskType = _taskType;
-            resourceType= _resourceType;
-            inputParam= _inputParam;
-            verifyParam= _verifyParam;
-            cpContractAddress= _cpContractAddress;
-            status = _status;
-            deadline = _deadline;
-            lockFundTx = _lockFundTx;
-        isAdmin[msg.sender] = true;
-    }
-
-    // Modifier to check if the caller is the admin
-    modifier onlyAdmin() {
-        require(isAdmin[msg.sender], "Only the admin can call this function.");
-        _;
-    }
-
-    function updateRewardAndStatus(
-        string memory _rewardTx,
-        string memory _status
-    ) public onlyAdmin {
-        rewardTx = _rewardTx;
-        status = _status;
-        emit RewardAndStatusUpdated(rewardTx, status);
-    }
-
-    function updateLockAndStatus(
-        string memory _lockFundTx,
-        string memory _status,
-        address _cpContractAddress
-    ) public onlyAdmin {
-        lockFundTx = _lockFundTx;
-        status = _status;
-        cpContractAddress = _cpContractAddress;
-        emit LockAndStatusUpdated(cpContractAddress, lockFundTx, status);
-    }
-
-    function updateUnlockAndStatus(
-        string memory _unlockFundTx,
-        string memory _status
-    ) public onlyAdmin {
-        unlockFundTx = _unlockFundTx;
-        status = _status;
-        emit UnlockAndStatusUpdated(unlockFundTx, status);
-    }
-
-    function updateChallengeAndStatus(
-        string memory _challengeTx,
-        string memory _status
-    ) public onlyAdmin {
-        challengeTx = _challengeTx;
-        status = _status;
-        isChallenged = true;
-        emit ChallengeAndStatusUpdated(challengeTx, status);
-    }
-
-    function updateSlashAndStatus(
-        string memory _slashTx,
-        string memory _status
-    ) public onlyAdmin {
-        slashTx = _slashTx;
-        status = _status;
-        emit SlashAndStatusUpdated(slashTx, status);
-    }
-
-    function submitProof(string memory _proof) public {
-        (bool success, bytes memory CPOwner) = cpContractAddress.call(abi.encodeWithSignature("getOwner()"));
-        require(success, "Failed to call getOwner function of CPAccount");
-        address owner = abi.decode(CPOwner, (address));
-
-        (bool ok, bytes memory CPWorker) = cpContractAddress.call(abi.encodeWithSignature("getWorker()"));
-        require(ok, "Failed to call getWorker function of CPAccount");
-        address worker = abi.decode(CPWorker, (address));
-
-        require(msg.sender == owner || msg.sender == worker, "Only the CP contract owner or worker can submit proof.");
-
+        address _cpAccount,
+        string memory _proof,
+        uint256 _deadline,
+        address _taskRegistryContract,
+        string memory _checkCode
+    ) {
+        taskID = _taskID;
+        taskType = _taskType;
+        resourceType = _resourceType;
+        inputParam = _inputParam;
+        verifyParam = _verifyParam;
+        cpAccount = _cpAccount;
         proof = _proof;
-        isSubmitted = true;
-        emit SubmitProof(proof);
+        deadline = _deadline;
+        taskRegistryContract = _taskRegistryContract;
+        checkCode = _checkCode;
+        owner = msg.sender;
+
+        // Register this task contract with the TaskRegistry
+        registerToTaskRegistry();
     }
 
-    function getTaskInfo() public view returns(TaskInfo memory) {
-        return TaskInfo(taskType, resourceType, inputParam,verifyParam, cpContractAddress,status,rewardTx,proof,challengeTx,lockFundTx,unlockFundTx,slashTx,deadline,isSubmitted,isChallenged);
+    // Private function to register this contract with the TaskRegistry
+    function registerToTaskRegistry() private {
+        (bool success, ) = taskRegistryContract.call(
+            abi.encodeWithSignature(
+                "registerTaskContract(address,address)",
+                address(this),
+                owner
+            )
+        );
+        require(success, "Failed to register task contract to TaskRegistry");
+        
+        // Emit the event after successful registration
+        emit RegisteredToTaskRegistry(address(this), owner);
     }
 
-    function version() public pure returns(string memory) {
-        return "1.0.0";
-    }
 }
